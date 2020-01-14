40ce6f832dc567e9dbe7cc3f33869eb67f0eae9a
ephellon <minkcbos@gmail.com>
2020-01-13 17:15:09 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,48 @@
+(async() => {
+    let $ = (selector, container = document) => [...container.querySelectorAll(selector)];
+
+    /* Adjust table widths */
+    $('table tr').forEach(tr => {
+        [...tr.children].forEach((child, index, array) => {
+            child.setAttribute('style', `width: ${ (100 / array.length) }%`);
+        });
+    });
+
+    /* Update the GitHub issue counter(s) */
+    let GitHubIssues;
+
+    await fetch('https://api.github.com/repos/SpaceK33z/web-to-plex/issues?state=open')
+        .then(response => response.json())
+        .then(issues => {
+            GitHubIssues = issues;
+
+            $('#github-open').forEach(tracker => {
+                tracker.innerHTML = issues.length;
+            });
+
+            $('#github-total').forEach(tracker => {
+                let latest = issues[0];
+
+                tracker.innerHTML = `<a href="${latest.html_url}" title="${latest.title}" target="_blank" :black>Latest Issue (#${latest.number})</a>`;
+            });
+        });
+
+    /* Update the rating counters */
+    $('[rating]').forEach(rating => {
+        let value = eval(rating.getAttribute('rating')),
+            stars = [];
+
+        if(!value)
+            return;
+
+        for(;value >= 1 && stars.length < 5; value--)
+            stars.push('full');
+        for(;value >= 0.5 && stars.length < 5; value -= 0.5)
+            stars.push('half');
+        for(;stars.length < 5;)
+            stars.push('zero');
+
+        for(let size of stars)
+            rating.innerHTML += `<img class="star icon" src="icon/${size}-star.png" />`;
+    });
+})();
